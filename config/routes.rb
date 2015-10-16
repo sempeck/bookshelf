1ef1aa2f4ba7a598da346174c13a9e990a0fdfbc
sempeck <jarek.sepek@mediorama.pl>
2015-10-16 21:55:47 +0200

    Database continued

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+
+  resources :books
+
+  # |map|
+  # map.resources :books 
+
+    # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
