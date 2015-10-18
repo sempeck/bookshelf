26b56a22e35216daf725a3370a711e1c9ae5dc7e
sempeck <jarek.sepek@mediorama.pl>
2015-10-18 18:10:36 +0200

    New book added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,26 @@ class BooksController < ApplicationController
   def index
     @books = Book.paginate :page => params[:page], :per_page => 10
   end
+
+  def new 
+    
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save 
+    redirect_to @book
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+    def book_params
+    	params.require(:book).permit(:title, :name, :surname, :title, :subtitle, :isbn, :color, :category, :tags, :lease, :cover)
+    end
+
 end
 
-# User.paginate(:page => 1).class
 
-
-
-
-# @books = Books.paginate :page => params[:page], :per_page => 10
